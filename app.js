4637cfdf6a7dc9028960c9b4621150d8a053095f
Daniel Kaluski <daniel.kaluski@aalto.fi>
2021-02-27 12:30:11 +0200

    Checking port number...

--- app.js
+++ app.js
@@ -28,8 +28,11 @@ app.use(router.routes());
 let port = 7777;
 //Make sure Heroku uses the correct port
 if (Deno.args.length > 0) {
-      const lastArgument = Deno.args[Deno.args.length - 1];  port = Number(lastArgument);
+    console.log("Changing port number!")
+      const lastArgument = Deno.args[Deno.args.length - 1];
+        port = Number(lastArgument);
     }
+console.log(`Port number is: ${port}`)
 app.listen({ port: port });
 
 /*if(!Deno.env.get("TEST_ENVIRONMENT")){
